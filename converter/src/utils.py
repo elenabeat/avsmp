892999443abc9ad3bfed85c88b99b3e66a70fa2b
Elena Jaramillo Rodriguez <ejaramillo701@gmail.com>
2024-10-05 09:00:51 -0700

    Converter setup (#2)
    * converter docker and directory setup
    
    * update dockerfiles and create converter class
    
    * run converter on test images
    
    * update gitkeep and gitignore
    
    * docstrings, example cleanup
    
    * update readme and run black
    
    * fix typos
    
    * add note to readme
    
    * more note

---
+++ converter/src/utils.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class VideoInfo:
+    """
+    Dataclass for storing video metadata
+    """
+
+    fps: float
+    duration: float
+    frameCount: int
+    frameTime: float
+    aspect_ratio: float
+    crop_args: Tuple[str, str]
+
+    def __repr__(self):
+        return f"VideoInfo(fps={self.fps}, duration={self.duration}, frameCount={self.frameCount}, frameTime={self.frameTime}, aspect_ratio={self.aspect_ratio})"
